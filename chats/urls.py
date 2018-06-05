7d8f4889dc7fc8c5d13cd53abf1a7e8d6068f420
Owlogona <artemdanilenko1997@gmail.com>
2018-06-05 17:12:30 +0300

    second

--- chats/urls.py
+++ chats/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 	path('', views.home_page, name="home_page"),
 	path('room/', views.room, name="home_page"),
-	path('get_messages/<dialog_id>/', views.get_messages, name='get_messages'),
-	path('get_user/<preference>/', views.get_user, name='get_user'),
+	path('get_messages/<int:dialog_id>/', views.get_messages, name='get_messages'),
+	path('get_user/<int:user_id>', views.get_user, name='get_user'),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import User, Dialog, Message
+from django.http import HttpResponse
+import json
 
 
 def home_page(request):
@@ -9,7 +11,11 @@ def room(request):
 	return render(request, 'chats/room.html', locals())
 
 def get_messages(request, dialog_id):
-	pass
+	message = Message.objects.get(dialog=dialog_id)
+	data = json.dumps(message)
+	return HttpResponse(data)
 
-def get_user(request, preference):
-	pass
+def get_user(request, user_id):
+	user = User.objects.get(user=user_id)
+	data = json.dumps(user)
+	return HttpResponse(data)
