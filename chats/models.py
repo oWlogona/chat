b1e95dca41a1d38d0f9bb5f09fb4bad46ef1d525
Owlogona <artemdanilenko1997@gmail.com>
2018-06-05 16:42:38 +0300

    second

--- chats/models.py
+++ chats/models.py
@@ -6,9 +6,9 @@ class User(models.Model):
 	sex = models.CharField(max_length=10)
 
 class Dialog(models.Model):
-	user_1 = models.ForeignKey(User, on_delete=models.CASCADE)
-	user_2 = models.CharField(max_length=32)
-	receiver = models.CharField(max_length=32)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	user_1 = models.CharField(max_length=32)
+	user_2 = models.CharField(max_length=32, default=None)
 
 class Message(models.Model):
 	dialog = models.ForeignKey(Dialog, on_delete=models.CASCADE)
