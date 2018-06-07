ab64181af5ed0b788b9113db6a1f7580d9373870
Owlogona <artemdanilenko1997@gmail.com>
2018-06-07 23:38:31 +0300

    js

--- chats/views.py
+++ chats/views.py
@@ -14,28 +14,36 @@ def home_page(request):
 
         i_user = User(sex=my_sex, age=my_age, name=my_sex)
         i_user.save(force_insert=True)
-        search_dialog = Dialog.objects.filter(user_1_age=my_age, user_1_sex=my_sex, user_1__age=partner_age, user_1__sex=partner_sex)
-        
+        search_dialog = Dialog.objects.filter(
+            user_1_age=my_age, user_1_sex=my_sex, user_1__age=partner_age, user_1__sex=partner_sex)
+
         if search_dialog:
-        	search_dialog = Dialog.objects.update(user_2=i_user)
-        	return render(request, 'room_page.html', locals())
-        dialog_id = Dialog.objects.create(user_1=i_user, user_1_age=partner_age, user_1_sex=partner_sex)
+            search_dialog = search_dialog.update(user_2=i_user, user_2_age=partner_age, user_2_sex=partner_sex)
+            answer = Dialog.objects.get(user_2=i_user)
+            user_id = answer.user_1.id
+            dialog_id = answer.id
+            url_data = '' + str(user_id) + '/' + str(dialog_id)
+            url = '/room/' + url_data
+            return HttpResponseRedirect(url)
+        dialog_id = Dialog.objects.create(
+            user_1=i_user, user_1_age=partner_age, user_1_sex=partner_sex)
 
         url_data = '' + str(i_user.id) + '/' + str(dialog_id.id)
-        url =  '/room/' + url_data
+        url = '/room/' + url_data
         return HttpResponseRedirect(url)
     return render(request, 'home_page.html', locals())
 
 
 def room(request, user_id, dialog_id):
     if request.method == "GET":
-        print('teee')
+        user_1_id = user_id
+        off_dialog_id = dialog_id
     return render(request, 'room_page.html', locals())
 
 
 def get_messages(request, dialog_id):
-    message = Message.objects.get(dialog=dialog_id)
-    data = json.dumps(message)
+    if request.method == 'GET':
+        print('test')
     return HttpResponse(data)
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,22 @@
-$.get( "test.php" );
+$(document).ready(function(){
+
+	var form = $('#chat_forms');
+
+	function get_message(){
+		var url_id = $('.hidden_input').val();
+		var url = 'get_messages/' + url_id + '/';
+		var data = {}
+		$.get(url, function(data) {
+			  $('.result').html(data);
+			  alert('Загрузка завершена.');
+			});
+	};
+
+	form.on('submit', function(e){
+		e.preventDefault();
+		console.log('Good')
+		get_message()
+		
+	})
+
+});
